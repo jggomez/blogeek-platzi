c4cb5888cd5d3f8233f1b015797d441967e4d7ab
jggomez <juan.gomez01@gmail.com>
2018-11-19 12:53:17 -0500

    se desarrolla la gestion del estado de los usuarios

--- public/js/general.js
+++ public/js/general.js
@@ -5,7 +5,7 @@ $(() => {
   // TODO: Adicionar el service worker
 
   // Init Firebase nuevamente
-  firebase.initializeApp(varConfig);
+  firebase.initializeApp(varConfig)
 
   // TODO: Registrar LLave publica de messaging
 
@@ -18,17 +18,36 @@ $(() => {
   // TODO: Listening real time
 
   // TODO: Firebase observador del cambio de estado
-  //$('#btnInicioSesion').text('Salir')
-  //$('#avatar').attr('src', user.photoURL)
-  //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
-  //$('#btnInicioSesion').text('Iniciar Sesión')
-  //$('#avatar').attr('src', 'imagenes/usuario.png')
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      $('#btnInicioSesion').text('Salir')
+      if (user.photoURL) {
+        $('#avatar').attr('src', user.photoURL)
+      } else {
+        $('#avatar').attr('src', 'imagenes/usuario_auth.png')
+      }
+    } else {
+      $('#btnInicioSesion').text('Iniciar Sesión')
+      $('#avatar').attr('src', 'imagenes/usuario.png')
+    }
+  })
 
   // TODO: Evento boton inicio sesion
   $('#btnInicioSesion').click(() => {
-    //$('#avatar').attr('src', 'imagenes/usuario.png')
-    // Materialize.toast(`Error al realizar SignOut => ${error}`, 4000)
-    
+    const user = firebase.auth().currentUser
+    if (user) {
+      $('#btnInicioSesion').text('Iniciar Sesión')
+      return firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          $('#avatar').attr('src', 'imagenes/usuario.png')
+          Materialize.toast(`SignOut Correcto`, 4000)
+        })
+        .catch(error => {
+          Materialize.toast(`Error al realizar SignOut => ${error}`, 4000)
+        })
+    }
 
     $('#emailSesion').val('')
     $('#passwordSesion').val('')
@@ -36,22 +55,24 @@ $(() => {
   })
 
   $('#avatar').click(() => {
-    firebase.auth().signOut()
-    .then(() => {
-      $('#avatar').attr('src', 'imagenes/usuario.png')
-      Materialize.toast(`SignOut correcto`, 4000)
-    })
-    .catch(error =>{
-      Materialize.toast(`Error al realizar SignOut ${error}`, 4000)
-    })
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        $('#avatar').attr('src', 'imagenes/usuario.png')
+        Materialize.toast(`SignOut correcto`, 4000)
+      })
+      .catch(error => {
+        Materialize.toast(`Error al realizar SignOut ${error}`, 4000)
+      })
   })
 
   $('#btnTodoPost').click(() => {
-    $('#tituloPost').text('Posts de la Comunidad')   
+    $('#tituloPost').text('Posts de la Comunidad')
   })
 
   $('#btnMisPost').click(() => {
-    //$('#tituloPost').text('Mis Posts')
-    //Materialize.toast(`Debes estar autenticado para ver tus posts`, 4000)    
+    // $('#tituloPost').text('Mis Posts')
+    // Materialize.toast(`Debes estar autenticado para ver tus posts`, 4000)
   })
 })
